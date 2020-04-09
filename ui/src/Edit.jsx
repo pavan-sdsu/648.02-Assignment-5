47fd1b1eb46c1175f0bba7bbb895a7fdac884b4c
pavan <pavan1645@gmail.com>
2020-04-09 13:04:46 -0700

    update ui

--- ui/src/Edit.jsx
+++ ui/src/Edit.jsx
@@ -1,13 +1,91 @@
 import React, { Component } from 'react'
+const BASE_URL = "http://localhost:3000";
 
 export default class Edit extends Component {
-	
-	render() {
-		const _id = this.props.location.pathname.split("/edit/")[1];
 
+	state = { product: {} }
+
+	constructor(props) {
+		super();
+		const _id = props.location.pathname.split("/edit/")[1];
+		this.getProduct(_id);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	getProduct(_id) {
+		const query = `
+		query {
+			getProduct (_id: "${_id}") {
+				_id
+				id
+				Category
+				Price
+				Name
+				Image
+			}
+		}
+		`;
+
+		fetch(BASE_URL + "/graphql", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ query })
+		})
+			.then(res => res.json())
+			.then((res) => {
+				this.setState((state, props) => {
+					state["product"] = res.data.getProduct;
+					return state;
+				})
+			})
+			.catch(err => console.error(err))
+	}
+
+	onSubmit(e) {
+		
+		const product = {
+			Category: document.getElementById("category").value,
+			Price: document.getElementById("price").value,
+			Name: document.getElementById("name").value,
+			Image: document.getElementById("image").value
+		}
+		console.log(product);
+		
+		e.preventDefault();
+		
+	}
+
+	render() {
+		const {Name, Price, Category, Image} = this.state.product;
 		return (
 			<div>
-				<h2>Hello Wordl</h2>
+				<h2>Update Product</h2>
+				<form>
+					<div className="input-group">
+						<label htmlFor="category">Category:</label>
+						<select name="category" id="category" defaultValue={Category}>
+							<option value="Accessories">Accessories</option>
+							<option value="Shirts">Shirts</option>
+							<option value="Jeans">Jeans</option>
+							<option value="Jackets">Jackets</option>
+							<option value="Sweaters">Sweaters</option>
+						</select>
+					</div>
+					<div className="input-group">
+						<label htmlFor="price">Price:</label>
+						<input type="text" id="price" defaultValue={Price} />
+					</div>
+					<div className="input-group">
+						<label htmlFor="name">Product Name:</label>
+						<input type="text" id="name" defaultValue={Name} />
+					</div>
+					<div className="input-group">
+						<label htmlFor="image">Image URL:</label>
+						<input type="text" id="image" defaultValue={Image} />
+					</div>
+
+					<button type="submit" onClick={this.onSubmit}>Update Product</button>
+				</form>
 			</div>
 		)
 	}
